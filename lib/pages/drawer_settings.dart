00cf88749f7fec6d64d18c53fc911666ca37b734
zhangbo <bo.zhang@tamigroup.com>
2019-12-06 22:57:53 +0800

    退出登录

--- lib/pages/drawer_settings.dart
+++ lib/pages/drawer_settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osc_client/common/event_bus.dart';
+import 'package:flutter_osc_client/constants/constants.dart';
+import 'package:flutter_osc_client/utils/data_utils.dart';
 
 class DrawerSettingPage extends StatefulWidget {
   @override
@@ -9,15 +12,33 @@ class _DrawerSettingPageState extends State<DrawerSettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
+        elevation: 0.0,
         //显示左侧的小箭头 默认true 显示
         automaticallyImplyLeading: true,
         iconTheme: IconThemeData(
-          color: Colors.white,
-        ) ,
+          color: Color(AppColors.APP_BAR),
+        ),
         title: Text(
-            "设置"
+          "设置",
+          style: TextStyle(
+            color: Color(AppColors.APP_BAR),
+          ),
+        ),
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            DataUtils.cleanLoginInfo().then((_){
+              eventBus.fire(LogoutEvent());
+              Navigator.of(context).pop("dissmiss_drawer");
+            });
+          },
+          child: Text(
+            "退出登录",
+            style: TextStyle(fontSize: 20.0
+            ),
+          ),
         ),
       ),
     );
